73a27c4b0eb64f388cb8f5565d6177cbdac7cf1c
Patricia Pellicer <ppellicer@gmx.ch>
2020-04-16 16:12:05 +0200

    changes made / layout to be improved

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+peking = {
+  name:          "Peking",
+  address:       "Hardstrasse 8, Zurich",
+  phone_number:  "01 04 35 24 76",
+  category:      "chinese"
+}
+Restaurant.create!(peking)
+
+rosso = {
+  name:          "Rosso",
+  address:       "Hardplatz 44, Zurich",
+  phone_number:  "01 45 34 27 56",
+  category:      "italian"
+}
+Restaurant.create!(rosso)
+
+wowotschsushi = {
+  name:          "Wo wotsch sus hi",
+  address:       "Langstrasse 34, Zurich",
+  phone_number:  "01 34 67 27 46",
+  category:      "japanese"
+}
+Restaurant.create!(wowotschsushi)
+
+filou = {
+  name:          "Filou",
+  address:       "Gasometerstrasse 78, Zurich",
+  phone_number:  "01 43 74 37 22",
+  category:      "french"
+}
+Restaurant.create!(filou)
+
+duvel = {
+  name:          "Duvel",
+  address:       "Limmatplatz 3, Zurich",
+  phone_number:  "01 35 35 99 20",
+  category:      "belgian"
+}
+Restaurant.create!(duvel)
+
+puts "Created restaurants"
+
+puts "Finished!"
